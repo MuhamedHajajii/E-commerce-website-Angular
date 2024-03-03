b4cb99221208ef9bec7e94ec3af782f81e962c78
haji <muhamedhajajii@outlook.com>
2024-03-03 10:36:00 +0200

    add online payment address suggestions

--- src/app/components/cash-payment/cash-payment.component.ts
+++ src/app/components/cash-payment/cash-payment.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { CartService } from 'src/app/shared/services/cart.service';
+import { UserAddressService } from '../user-address.service';
+import { Address } from '../address';
+import { ToastrService } from 'ngx-toastr';
+import { AuthServisesService } from 'src/app/shared/services/auth-servises.service';
 
 @Component({
   selector: 'app-cash-payment',
@@ -13,10 +17,14 @@ export class CashPaymentComponent {
     private _CartService: CartService,
     private _ActivatedRoute: ActivatedRoute,
     private _FormBuilder: FormBuilder,
-    private _Router: Router
+    private _Router: Router,
+    private _UserAddressService: UserAddressService,
+    private _ToastrService: ToastrService,
+    private _AuthServisesService: AuthServisesService
   ) {}
   ngOnInit(): void {
     this.getCurrentCurtId();
+    this.getUserAddress();
   }
 
   userAddress: FormGroup = this._FormBuilder.group({
@@ -40,6 +48,7 @@ export class CashPaymentComponent {
 
   confirmUserAddress(): void {
     this.loading = true;
+    this.getUserName();
     this._CartService
       .checkOutSessionCash(this.currentId, this.userAddress.value)
       .subscribe({
@@ -49,5 +58,49 @@ export class CashPaymentComponent {
           this.loading = false;
         },
       });
+  }
+
+  currentUserAddress!: Address;
+
+  getUserAddress(): void {
+    this._UserAddressService.getUserAddress().subscribe({
+      next: (response) => {
+        this.currentUserAddress = response;
+      },
+    });
+  }
+  currentAddress(e: any): void {
+    let selected = e.target.value;
+    this.currentUserAddress.data.forEach((element) => {
+      if (element._id == selected) {
+        this.userAddress.get('details')?.setValue(element.details);
+        this.userAddress.get('phone')?.setValue(element.phone);
+        this.userAddress.get('city')?.setValue(element.city);
+      }
+    });
+  }
+
+  userName: string = '';
+  getUserName(): void {
+    this._AuthServisesService.userName.subscribe({
+      next: (response) => {
+        this.userName = response;
+        this.addNewAddress(response);
+      },
+    });
+  }
+
+  addNewAddress(userName: string): void {
+    let userDetails = this.userAddress.value;
+    userDetails.name = userName + 'is Address';
+
+    this._UserAddressService.addNewAddress(userDetails).subscribe({
+      next: (response) => {
+        this.currentUserAddress = response;
+        this._ToastrService.show(
+          'Order has been Done successfully <i class="text-main fa-2x fa-solid fa-house-circle-check"></i>'
+        );
+      },
+    });
   }
 }
--- src/app/components/online-payment/online-payment.component.ts
+++ src/app/components/online-payment/online-payment.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { CartService } from 'src/app/shared/services/cart.service';
+import { UserAddressService } from '../user-address.service';
+import { ToastrService } from 'ngx-toastr';
+import { AuthServisesService } from 'src/app/shared/services/auth-servises.service';
+import { Address } from '../address';
 
 @Component({
   selector: 'app-online-payment',
@@ -12,10 +16,14 @@ export class OnlinePaymentComponent implements OnInit {
   constructor(
     private _CartService: CartService,
     private _ActivatedRoute: ActivatedRoute,
-    private _FormBuilder: FormBuilder
+    private _FormBuilder: FormBuilder,
+    private _UserAddressService: UserAddressService,
+    private _ToastrService: ToastrService,
+    private _AuthServisesService: AuthServisesService
   ) {}
   ngOnInit(): void {
     this.getCurrentCurtId();
+    this.getUserAddress();
   }
 
   userAddress: FormGroup = this._FormBuilder.group({
@@ -39,6 +47,7 @@ export class OnlinePaymentComponent implements OnInit {
 
   confirmUserAddress(): void {
     this.loading = true;
+    this.getUserName();
     this._CartService
       .checkOutSession(this.currentId, this.userAddress.value)
       .subscribe({
@@ -47,5 +56,48 @@ export class OnlinePaymentComponent implements OnInit {
           window.location.assign(response.session.url);
         },
       });
+  }
+  currentUserAddress!: Address;
+
+  getUserAddress(): void {
+    this._UserAddressService.getUserAddress().subscribe({
+      next: (response) => {
+        this.currentUserAddress = response;
+      },
+    });
+  }
+  currentAddress(e: any): void {
+    let selected = e.target.value;
+    this.currentUserAddress.data.forEach((element) => {
+      if (element._id == selected) {
+        this.userAddress.get('details')?.setValue(element.details);
+        this.userAddress.get('phone')?.setValue(element.phone);
+        this.userAddress.get('city')?.setValue(element.city);
+      }
+    });
+  }
+
+  userName: string = '';
+  getUserName(): void {
+    this._AuthServisesService.userName.subscribe({
+      next: (response) => {
+        this.userName = response;
+        this.addNewAddress(response);
+      },
+    });
+  }
+
+  addNewAddress(userName: string): void {
+    let userDetails = this.userAddress.value;
+    userDetails.name = userName + 'is Address';
+
+    this._UserAddressService.addNewAddress(userDetails).subscribe({
+      next: (response) => {
+        this.currentUserAddress = response;
+        this._ToastrService.show(
+          'Order has been Done successfully <i class="text-main fa-2x fa-solid fa-house-circle-check"></i>'
+        );
+      },
+    });
   }
 }
